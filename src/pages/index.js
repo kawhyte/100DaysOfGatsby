4083796a4b7d1f08d0566ed8cd2bc5dc3b95b639
Kenny <kawhyte@gmail.com>
2020-01-15 19:11:40 -0800

    Links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "./components/header"
+
 export default () => (
-  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello Kenny!" />
+  <p> We are here</p>
+  <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
 )
